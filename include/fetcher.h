be372aecf9225ceeea8001eb60cd84122dbddb9e
Arnav Parashar <arnavparashar408@gmail.com>
2026-08-06 12:26:40 +0530

    chore: partial initial setup

---
+++ include/fetcher.h
@@ -0,0 +1,9 @@
+#ifndef FETCHER_H
+#define FETCHER_H
+
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/frame.h>
+
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "audio.h"
+#include "decoder.h"
+#include "fetcher.h"
+#include "player.h"
+#include "renderer.h"
+#include "terminal.h"
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>       
+#include <libavutil/avutil.h>         
+#include <libavutil/frame.h>         
+#include <libavutil/error.h>          
+#include <libswscale/swscale.h>       
+
+int main(int argc, char *argv[]){
+
+}
