3cce4801af07deb5a0ef8f356a1a8a05b167f18d
Arnav Parashar <arnavparashar408@gmail.com>
2026-08-12 23:56:24 +0530

    feat: decoder init

--- include/decoder.h
+++ include/decoder.h
@@ -1,5 +1,21 @@
 #ifndef DECODER_H
 #define DECODER_H
 
+#include <libavcodec/avcodec.h>
+#include <libavutil/frame.h>
+
+#include "fetcher.h"
+typedef struct{
+    const AVCodec *codec;
+    AVCodecContext *codecContext;
+} Decoder;
+
+int decoderInit(Decoder *decoder, Fetcher *fetcher);
+
+int decoderSendPacket(Decoder *decoder, AVPacket *packet);
+
+int decoderReceiveFrame(Decoder *decoder, AVFrame *frame);
+
+void decoderDestroy(Decoder *decoder);
 
 #endif
---
+++ src/decoder.c
@@ -0,0 +1,41 @@
+#include "decoder.h"
+#include "fetcher.h"
+#include <libavcodec/avcodec.h>
+#include <libavutil/frame.h>
+
+int decoderInit(Decoder *decoder, Fetcher *fetcher){
+    decoder->codec = NULL;
+    decoder->codecContext = NULL;
+    AVStream *stream = fetcher->frmtcontext->streams[fetcher->vStreamIndex];
+    AVCodecParameters *codecpar = stream->codecpar;
+    if (decoder->codec == NULL) {
+        return -5; //btw this type of error handling will be changed in future, ik it is shit rn but there are more imporant things that need to be impleemnted rather than a proper error handling suite rn
+    }
+
+    decoder->codecContext = avcodec_alloc_context3(decoder->codec);
+
+    if (decoder->codecContext == NULL) {
+        return -6;
+    } 
+    int ret = avcodec_parameters_to_context(decoder->codecContext,codecpar);
+    if (ret < 0) {
+        avcodec_free_context(&decoder->codecContext);
+        return -7;
+    }
+
+    ret = avcodec_open2(decoder->codecContext,decoder->codec,NULL);
+
+    if (ret < 0) {
+        avcodec_free_context(&decoder->codecContext);
+        return -8;
+    }
+
+    return 0;
+
+}
+
+int decoderSendPacket(Decoder *decoder, AVPacket *packet);
+
+int decoderReceiveFrame(Decoder *decoder, AVFrame *frame);
+
+void decoderDestroy(Decoder *decoder);
