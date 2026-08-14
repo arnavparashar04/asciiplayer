f5fafb8c27900a260d003239cda00c3d7518edd3
Arnav Parashar <arnavparashar408@gmail.com>
2026-08-15 01:30:33 +0530

    feat: initial renderer structure

--- include/renderer.h
+++ include/renderer.h
@@ -1,7 +1,30 @@
 #ifndef RENDERER_H
 #define RENDERER_H
 
-typedef struct{
+#include <libavutil/frame.h>
+
+typedef struct {
+    //src res 
+    int frameWidth;
+    int frameHeight;
+    //target res 
+    int targetWidth;
+    int targetHeight;
+
+    int charCellWidth;
+    int CharCellHeight;
+
+    const char *charMap;
+    int charMapLen;
+    struct SwsContext *swscont;
+    AVFrame *frame;
+    char *output;
 } Renderer;
+
+int rendererinit(Renderer *renderer);
+int rendererComp(Renderer *renderer);
+int normalize(Renderer *renderer, AVFrame *frame);
+int render(Renderer *renderer, AVFrame *frame);
+void rDestroy(Renderer *renderer);
 
 #endif
---
+++ src/renderer.c
@@ -0,0 +1,10 @@
+#include "renderer.h"
+#include <libavutil/imgutils.h>
+#include <libavutil/pixfmt.h>
+#include <libswscale/swscale.h>
+#include <stdlib.h>
+#include <string.h>
+
+int rendererinit(){
+
+}
