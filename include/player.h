38419b4426bab13a898be5b9d51ea3da9328ddaf
Arnav Parashar <arnavparashar408@gmail.com>
2026-08-14 11:48:34 +0530

    chore: add basic structure for the headers

---
+++ include/player.h
@@ -0,0 +1,21 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include"fetcher.h"
+#include "decoder.h"
+#include "renderer.h"
+#include "terminal.h"
+#include "audio.h"
+
+
+typedef struct{
+
+    Fetcher fetcher;
+    Decoder decoder;
+    Renderer renderer;
+    Terminal terminal;
+    Audio audio;
+
+}Player;
+
+#endif
