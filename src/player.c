d92e1eb0b269a6d104801726be56bc189deb2242
Arnav Parashar <arnavparashar408@gmail.com>
2026-08-17 00:20:15 +0530

    feat: most main parts of the player loop

--- src/player.c
+++ src/player.c
@@ -4,26 +4,28 @@
 #include "renderer.h"
 #include "terminal.h"
 #include "audio.h"
+#include <asm-generic/errno-base.h>
 #include <libavcodec/avcodec.h>
 #include <libavcodec/packet.h>
 #include <libavutil/error.h>
 #include <libavutil/frame.h>
 
 int playerinit(Player *player, const char *path){
-    int retrned = fetcherInit(player->fetcher,path);
+    int retrned = fetcherInit(&player->fetcher,path);
     if(retrned != 0){
         return -50; //todo : here in error enum map to critical fetcher failure
     }
-    retrned = decoderInit(player->decoder,player->fetcher);
+    retrned = decoderInit(&player->decoder,&player->fetcher);
     if(retrned!=0){
         return -51; //todo :here in error enum map to critical decoder failure
     }
    
-    retrned = rendererinit(player->renderer);
+    retrned = rendererinit(&player->renderer);
     if(retrned !=0){
         return -52; //todo: in error enum map to critical renderer failure
     }
-
+    
+    return 0;
 }
 
 int playerPlay(Player *player){
@@ -39,16 +41,33 @@ int playerPlay(Player *player){
        if(packet->stream_index != player->fetcher.vStreamIndex){
            av_packet_unref(packet);
        }
-       int rtrned = avcodec_send_packet(player->decoder.codecContext, packet); //here it is actually sent to the decoder with codec context so decoder will know how it should decode                                                   
+       int rtrned = avcodec_send_packet(player->decoder.codecContext, packet); //here is where it is actually sent to the decoder with codec context so decoder will know how it should decode                                                   
        av_packet_unref(packet);
        if(rtrned <0){
-           return -100; //todo: map in error enum to criticl decoder error
+           return -100; //todo: map in error enum to criticl decoder failure
        }
 
+       while(1){
+           //this loop may need to modified for play/pause operations
+           rtrned = avcodec_receive_frame(player->decoder.codecContext,frame);
+           if(rtrned == AVERROR(EAGAIN)|| rtrned == AVERROR_EOF || rtrned == AVERROR_EXIT){
+               break;
+           }
+           if(rtrned < 0 ){
+               av_frame_free(&frame);
+               av_packet_free(&packet);
+
+               return -101; // todo: map to critcial player failure
+           }
+
+           render(&player->renderer,frame);
+       }
    }
 
    av_packet_free(&packet);
    av_frame_free(&frame);
+
+   return 0;
 }
 
 
