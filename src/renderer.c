1c3f0182ae12efa67922c825173bc3cd3072103d
Arnav Parashar <arnavparashar408@gmail.com>
2026-08-18 23:22:39 +0530

    chore: renderer normalize

--- src/renderer.c
+++ src/renderer.c
@@ -3,8 +3,11 @@
 #include <libavutil/imgutils.h>
 #include <libavutil/pixfmt.h>
 #include <libswscale/swscale.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define ALIGNMENT 32
 
 int rendererinit(Renderer *renderer, int frameWidth, int frameHeight){
    renderer->frameWidth = frameWidth;
@@ -13,7 +16,6 @@ int rendererinit(Renderer *renderer, int frameWidth, int frameHeight){
    renderer->targetHeight = frameHeight; //upto v1 frame res = target res
    renderer->charCellWidth = 5;
    renderer->charCellHeight = 10;
-
    renderer->charMap = ".`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
    renderer->charMapLen = strlen(renderer->charMap);
    renderer->swscontxt = NULL;
@@ -26,7 +28,24 @@ int rendererComp(Renderer *renderer){
 }
 
 int normalize(Renderer *renderer, AVFrame *frame){
-
+    if(renderer->swscontxt == NULL){
+        renderer->swscontxt = sws_getContext(renderer->frameWidth, renderer->frameHeight, frame->format, renderer->frameWidth, renderer->frameHeight, AV_PIX_FMT_GRAY8, SWS_BILINEAR, NULL, NULL, NULL);
+        if(renderer->swscontxt == NULL){
+            return -150; // error map to failed to get context 
+        }
+        renderer->normframe = av_frame_alloc();
+        if(renderer->normframe == NULL){
+            return -151; // map to norm frame init failed
+        }
+        renderer->normframe->format = AV_PIX_FMT_GRAY8;
+        renderer->normframe->width = renderer->frameWidth;
+        renderer->normframe->height = renderer->frameHeight;
+        if(av_frame_get_buffer(renderer->normframe, ALIGNMENT) < 0){
+            return -152; //map to norm frame alloc failed
+        }
+    }
+    sws_scale(renderer->swscontxt, (const uint8_t *const *)frame->data, frame->linesize, 0, frame->height, renderer->normframe->data, renderer->normframe->linesize);
+    return 0;
 }
 
 int render(Renderer *renderer, AVFrame *frame){
