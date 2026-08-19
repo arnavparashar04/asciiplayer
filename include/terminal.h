297c6b5fdeb358e92be5e73054d174767ba77f14
Arnav Parashar <arnavparashar408@gmail.com>
2026-08-19 23:27:37 +0530

    chore: terminal header

--- include/terminal.h
+++ include/terminal.h
@@ -2,6 +2,12 @@
 #define TERMINAL_H
 
 typedef struct{
+    int width;
+    int height;
 } Terminal;
+
+int terminalinit(Terminal *terminal);
+void terminalRender(Terminal *terminal, char *outbuffer);
+void terminaldestroy(Terminal *terminal);
 
 #endif
