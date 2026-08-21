f60cb9702b0a10924f15a4e75d359256a3218e92
Arnav Parashar <arnavparashar408@gmail.com>
2026-08-21 23:36:04 +0530

    chore: update help message

--- src/main.c
+++ src/main.c
@@ -7,21 +7,12 @@ int main(int argc, char *argv[]){
     cliArgs args = parseArgs(argc, argv);
     if(args.askHelp){
         printf(
-            "Usage: avplayer <path> [options]\n"
+            "Usage:  asciiplayer <path> or <options>\n"
             "\n"
             "Options:\n"
             "  -h,  --help              Show this help message\n"
             "  -v,  --version           Show version information\n"
-            "  -d,  --dirty             Enable dirty updates  (Status : Work In Progress)\n"
-            "  -g,  --glyph             Enable glyph matching  (Status : Work In Progress)\n"
-            "  -oc, --offcolor          Disable colour output\n"
-            "      --fps <number>       Set playback FPS (default: 30)  (Status : Work In Progress)\n"
-            "      --width <number>     Set output width (default: 1300)  (Status : Work In Progress)\n"
-            "      --height <number>    Set output height (default: 760)  (Status : Work In Progress)\n"
-            "\n"
-            "Example:\n"
-            "  ascii-video video.mp4\n"
-        );
+            );
         return 0;
     }
     if(args.askVersion){
